dc6d8023fd5e5240dd4510e4802e21f9fd97ceac
PseudoscienceGames <quade.81@gmail.com>
2017-02-14 19:26:10 -0600

    Added death removed run added spawn points added timer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,6 @@ public class Player : MonoBehaviour
 	public Vector3 wjLeap;
 	public float wallSpeed = 3;
 	public float wallStickTime;
-	public float runMultiplier;
 	public float jumpLeeway;
 
 	float timeSinceGrounded = 0;
@@ -28,89 +27,79 @@ public class Player : MonoBehaviour
 	public Vector3 velocity;
 	Vector3 normal;
 	CharacterController pc;
+	bool isAlive = true;
+	public GameObject deadPlayer;
 
 	void Start()
 	{
 		pc = GetComponent<CharacterController>();
-		//gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-		//maxJumpVelocity = Mathf.Abs(gravity * timeToJumpApex);
-		//minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+		gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+		maxJumpVelocity = Mathf.Abs(gravity * timeToJumpApex);
+		minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
 	}
 
 	void Update()
 	{
-		//===================================================================
-		gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-		maxJumpVelocity = Mathf.Abs(gravity * timeToJumpApex);
-		minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
-		//===================================================================
-		if (pc.isGrounded)
+		if (isAlive)
 		{
-			timeSinceGrounded = 0;
-			velocity.y = -0.1f;
-		}
-		else
-			timeSinceGrounded += Time.deltaTime;
-		Vector3 input = Camera.main.transform.root.TransformDirection(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * moveSpeed);
-		if (Input.GetButton("Run"))
-			input *= runMultiplier;
-		Vector3 target = input;
-		if (timeToWallUnstick > 0)
-			timeToWallUnstick -= Time.deltaTime;
-		if (Input.GetButtonDown("Jump"))
-		{
-			if (pc.isGrounded || timeSinceGrounded <= jumpLeeway)
+			if (pc.isGrounded)
 			{
-				velocity.y = maxJumpVelocity;
-				timeSinceGrounded = jumpLeeway + 1;
+				timeSinceGrounded = 0;
+				velocity.y = -0.1f;
 			}
-			else if (timeToWallUnstick > 0)
+			else
+				timeSinceGrounded += Time.deltaTime;
+			Vector3 input = Camera.main.transform.root.TransformDirection(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * moveSpeed);
+			Vector3 target = input;
+			if (timeToWallUnstick > 0)
+				timeToWallUnstick -= Time.deltaTime;
+			if (Input.GetButtonDown("Jump"))
 			{
-				timeToWallUnstick = 0;
-				if (input.x == 0 && input.z == 0)//no movement
+				if (pc.isGrounded || timeSinceGrounded <= jumpLeeway)
 				{
-					velocity = normal * wjOff.z;
-					velocity.y = wjOff.y;
+					velocity.y = maxJumpVelocity;
+					timeSinceGrounded = jumpLeeway + 1;
 				}
-				else if (Vector3.Angle(normal, input) < 90)//Backward movement
+				else if (timeToWallUnstick > 0)
 				{
-					velocity = (normal + input).normalized * wjLeap.z;
-					velocity.y = wjLeap.y;
-				}
-				else//forward movement
-				{
-					velocity = normal * wjClimb.z;//(normal + input).normalized * wjClimb.z;
-					velocity.y = wjClimb.y;
+					timeToWallUnstick = 0;
+					if (input.x == 0 && input.z == 0)//no movement
+					{
+						velocity = normal * wjOff.z;
+						velocity.y = wjOff.y;
+					}
+					else if (Vector3.Angle(normal, input) < 90)//Backward movement
+					{
+						velocity = (normal + input).normalized * wjLeap.z;
+						velocity.y = wjLeap.y;
+					}
+					else//forward movement
+					{
+						velocity = normal * wjClimb.z;//(normal + input).normalized * wjClimb.z;
+						velocity.y = wjClimb.y;
+					}
 				}
 			}
-		}
-		//if (timeToWallUnstick > 0 && velocity.y < 0)
-		//{
-		//	Debug.Log(input + " " + normal);
-		//	//velocity.x = -normal.x + (input.x / (moveSpeed / 2f));
-		//	//velocity.z = -normal.z + (input.z / (moveSpeed / 2f));
-		//	float oldY = velocity.y;
-		//	velocity = Vector3.ProjectOnPlane(new Vector3(input.x, velocity.y, input.z), normal);
-		//	velocity = ((velocity.normalized * 2) - normal.normalized).normalized * velocity.magnitude;
-		//	velocity.y = oldY;
-		//}
-		if (Input.GetButtonUp("Jump") && velocity.y > minJumpVelocity)
+			if (Input.GetButtonUp("Jump") && velocity.y > minJumpVelocity)
 				velocity.y = minJumpVelocity;
-		
-		target.y = velocity.y;
-		float smoothSpeed;
-		if (pc.isGrounded)
-			smoothSpeed = groundSmooth;
-		else
-			smoothSpeed = airSmooth;
-		velocity = Vector3.SmoothDamp(velocity, target, ref smoothdamp, smoothSpeed);
-		velocity.y += gravity * Time.deltaTime;
-		transform.LookAt(new Vector3(transform.position.x + velocity.x, transform.position.y, transform.position.z + velocity.z));
-		pc.Move(velocity * Time.deltaTime);
+
+			target.y = velocity.y;
+			float smoothSpeed;
+			if (pc.isGrounded)
+				smoothSpeed = groundSmooth;
+			else
+				smoothSpeed = airSmooth;
+			velocity = Vector3.SmoothDamp(velocity, target, ref smoothdamp, smoothSpeed);
+			velocity.y += gravity * Time.deltaTime;
+			transform.LookAt(new Vector3(transform.position.x + velocity.x, transform.position.y, transform.position.z + velocity.z));
+			pc.Move(velocity * Time.deltaTime);
+		}
 	}
 
 	private void OnControllerColliderHit(ControllerColliderHit hit)
 	{
+		if (hit.gameObject.tag == "Lethal")
+			Die();
 		normal = hit.normal;
 		if (!pc.isGrounded && hit.normal.y < 0.1f && hit.normal.y > -0.5f)
 		{
@@ -122,5 +111,31 @@ public class Player : MonoBehaviour
 		{
 			velocity.y = gravity * Time.deltaTime;
 		}
+	}
+
+	void Die()
+	{
+		isAlive = false;
+		Explode();
+		Invoke("Respawn", 1);
+	}
+
+	void Explode()
+	{
+		transform.FindChild("Sphere").gameObject.SetActive(false);
+		velocity = Vector3.zero;
+		GameObject dp = Instantiate(deadPlayer, transform.position, transform.rotation) as GameObject;
+		foreach(Rigidbody rb in dp.GetComponentsInChildren<Rigidbody>())
+		{
+			rb.AddForce(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * 100);
+		}
+	}
+
+	void Respawn()
+	{
+		transform.position = GameObject.FindGameObjectWithTag("Spawn").transform.position;
+		GameObject.Find("Timer").GetComponent<Timer>().Reset();
+		isAlive = true;
+		transform.FindChild("Sphere").gameObject.SetActive(true);
 	}
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+	public float time;
+
+	void Update()
+	{
+		time += Time.deltaTime;
+	}
+
+	public void Reset()
+	{
+		time = 0;
+	}
+}
